c13d3555669dc1015ee803e69e30a2c2dda71827
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-05-07 21:21:55 -0500

    Archivos bloque 03 parte 02

--- BLOQUE-03/Parte02.sql
+++ BLOQUE-03/Parte02.sql
@@ -6,8 +6,20 @@ isnull(a.fecfin,'9999-12-31') as [FECHA FIN]
 from Trabajador t left join Asignacion a on t.idtrabajador=a.idencuestador
 where t.tipo='E' and t.estado=1
 
-select * from Asignacion
-where idencuestador in (49,50)
+--03.07
 
-select * from Trabajador
-where idtrabajador in (49,50)
+select m.nombre as MANZANA,case when m.estado=1 then 'ACTIVO' else 'INACTIVO' end as ESTADO,
+m.idsector as IDSECTOR,isnull(a.idencuestador,0) as IDENCUESTADOR
+from Manzana m left join Asignacion a on m.idmanzana=a.idmanzana
+order by m.estado
+
+--03.08
+select t.usuario as USUARIO,t.idtrabajador as [ID ENCUESTADOR],
+p.nombres+' '+p.apellidos as NOMBRE_COMPLETO,
+isnull(a.idmanzana,0) as [ID MANZANA],isnull(a.fecinicio,'9999-12-31') as [FECHA INICIO],
+isnull(a.fecfin,'9999-12-31') as [FECHA FIN]
+from Trabajador t 
+left join Asignacion a on t.idtrabajador=a.idencuestador
+--NOMBRE_COMPLETO
+inner join Padron p on t.idpadron=p.idpadron
+where t.tipo='E' and t.estado=1
