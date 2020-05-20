8c2ce68f9866ae2099b7c63e1a4b022c119893bd
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-05-19 20:18:55 -0500

    Examen tipo 02

--- TALLER/TIPO_EXAMEN_02.sql
+++ TALLER/TIPO_EXAMEN_02.sql
@@ -15,3 +15,42 @@ order by t.numero asc
 --Sólo los registros de la posición 101 a la 120
 offset 100 rows
 fetch next 20 rows only
+
+--02
+
+select 
+case 
+when cl.tipocliente=1 then cl.nombres+' '+cl.apellidopat+' '+cl.apellidomat
+when cl.tipocliente=2 then razonsocial 
+else '-' end as cliente,
+isnull(p.nombre,'-') as [PLAN],
+co.feccontrato,co.ssisredwifi,
+isnull(u.nom_dpto,'***') as DPTO,
+isnull(u.nom_prov,'***') as PROVINCIA,
+isnull(u.nom_dto,'***') as DISTRITO
+/*Liste los contratos, independientemente de que los contratos cuenten 
+con clientes y planes relacionados*/
+from Contrato co 
+left join Cliente cl on co.idcliente=cl.idcliente
+left join PlanWifi p on co.idplan=p.idplan
+left join Zona z on cl.idzona=z.idzona
+left join Ubigeo u on z.idubigeo=u.idubigeo
+--Presentar solamente los contratos de facturación mensual ‘M’.
+where co.periodofactura='M'
+--Ordenados por id-plan (Menor a mayor) + id-cliente (Menor a mayor).
+order by co.idplan asc, co.idcliente asc
+
+select * from Contrato
+
+--03
+
+--03.01 Teléfonos de Comercial con id de teléfono entre 100 y 200
+select * from comercial.Telefono where idtelefono between 100 and 200
+
+--03.02 Insertar en Teléfonos de Sistemas lo de 03.01
+begin tran
+insert into dbo.Telefono(tipo,numero,idcliente,estado)
+select tipo,numero,idcliente,estado from comercial.Telefono where idtelefono between 100 and 200
+rollback
+
+select count(1) from Telefono
