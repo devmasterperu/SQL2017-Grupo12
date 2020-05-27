0f4d07b7321242012f4f35881486d72d6d3c0f96
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-05-26 20:49:28 -0500

    Archivos bloque 06

--- BLOQUE-06/Parte01.sql
+++ BLOQUE-06/Parte01.sql
@@ -18,12 +18,32 @@ left join (
 order by rm.nombre
 
 --OVER+FUNCIONES AGRUPAMIENTO
+create function F_REPORTE_FICHA() returns table as
+return
+	select idficha as [ID FICHA],numhabitantes as [NUM HAB],m.nombre as [NOM MZA],
+	count(idficha)     OVER(PARTITION BY m.nombre) as [M NUM FICHAS],
+	sum(numhabitantes) OVER(PARTITION BY m.nombre) as [M TOT HAB],
+	max(numhabitantes) OVER(PARTITION BY m.nombre) as [M MAX HAB],
+	min(numhabitantes) OVER(PARTITION BY m.nombre) as [M MIN HAB],
+	avg(numhabitantes) OVER(PARTITION BY m.nombre) as [M PROM HAB]
+	from Ficha f left join Manzana m on f.idmanzana=m.idmanzana
 
-select idficha as [ID FICHA],numhabitantes as [NUM HAB],m.nombre as [NOM MZA],
-count(idficha)     OVER(PARTITION BY m.nombre) as [M NUM FICHAS],
-sum(numhabitantes) OVER(PARTITION BY m.nombre) as [M TOT HAB],
-max(numhabitantes) OVER(PARTITION BY m.nombre) as [M MAX HAB],
-min(numhabitantes) OVER(PARTITION BY m.nombre) as [M MIN HAB],
-avg(numhabitantes) OVER(PARTITION BY m.nombre) as [M PROM HAB]
-from Ficha f left join Manzana m on f.idmanzana=m.idmanzana
-order by m.nombre
+select * from F_REPORTE_FICHA() order by [NOM MZA]
+
+--06.03
+	create function F_REPORTE_MANZANA(@idmanzana int) returns table as
+	alter function F_REPORTE_MANZANA(@idmanzana int) returns table as
+	return
+	select 
+	m.nombre as [MANZANA],
+	idficha as [IDFICHA],
+	numhabitantes as [NHABITANTES],
+	ROW_NUMBER()OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as [RN],
+	RANK()OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as [RK],
+	DENSE_RANK()OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as [DRK],
+	NTILE(4)OVER(PARTITION BY m.nombre ORDER BY f.numhabitantes ASC) as NTILE4
+	from Ficha f left join Manzana m on f.idmanzana=m.idmanzana
+	where f.idmanzana=case when @idmanzana=0 then f.idmanzana else @idmanzana end
+
+	select * from F_REPORTE_MANZANA(0)
+	--order by m.nombre asc, f.numhabitantes ASC
