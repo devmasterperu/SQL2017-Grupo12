af683fa91919808d88c02e2bb9ef754ba3595320
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-05-26 21:58:16 -0500

    Archivos bloque 06

--- BLOQUE-06/Parte01.sql
+++ BLOQUE-06/Parte01.sql
@@ -47,3 +47,65 @@ select * from F_REPORTE_FICHA() order by [NOM MZA]
 
 	select * from F_REPORTE_MANZANA(0)
 	--order by m.nombre asc, f.numhabitantes ASC
+
+--06.04
+create function F_REPORTE_TIPO(@tipoconsumidor char(1)) returns table as
+return
+	select  tipoconsumidor as TIPO,
+			idficha as IDFICHA,
+			montopago as MTOPAGO,
+			ROW_NUMBER() OVER(PARTITION BY tipoconsumidor ORDER BY montopago DESC) as RN,
+			RANK() OVER(PARTITION BY tipoconsumidor ORDER BY montopago DESC) as RK,
+			DENSE_RANK() OVER(PARTITION BY tipoconsumidor ORDER BY montopago DESC) as DRK,
+			NTILE(5) OVER(PARTITION BY tipoconsumidor ORDER BY montopago DESC) as NTILE5,
+			NTILE(10) OVER(PARTITION BY tipoconsumidor ORDER BY montopago DESC) as NTILE10,
+			NTILE(15) OVER(PARTITION BY tipoconsumidor ORDER BY montopago DESC) as NTILE15
+	from Ficha 
+	where tipoconsumidor=case when @tipoconsumidor='T' then tipoconsumidor else @tipoconsumidor end
+
+	select * from F_REPORTE_TIPO('T')
+    order by TIPO,MTOPAGO desc
+
+--06.05
+
+--FORMA_01: SUBCONSULTA EN SELECT
+select t.usuario as USUARIO, ltrim(p.nombres) as NOMBRES, ltrim(p.apellidos) as APELLIDOS,
+(select count(idficha) from Ficha f where f.idtrabajador=t.idtrabajador) as TOTAL,
+ROW_NUMBER() OVER(ORDER BY (select count(idficha) from Ficha f where f.idtrabajador=t.idtrabajador) DESC)as RN
+from Trabajador t left join Padron p on t.idpadron=p.idpadron
+where t.tipo='E'
+
+--FORMA_02: SUBCONSULTA EN FROM
+select t.usuario as USUARIO, ltrim(p.nombres) as NOMBRES, ltrim(p.apellidos) as APELLIDOS,
+(select count(idficha) from Ficha f where f.idtrabajador=t.idtrabajador) as TOTAL,
+ROW_NUMBER() OVER(ORDER BY rt.total DESC)as RN
+from Trabajador t left join Padron p on t.idpadron=p.idpadron
+left join (
+	select idtrabajador,count(idficha) as total
+    from Ficha f 
+    group by idtrabajador
+) rt on t.idtrabajador=rt.idtrabajador
+where t.tipo='E'
+
+--FORMA_03: CTE
+ALTER VIEW V_REPORTE_ENCUESTADOR AS
+WITH CTE_RT AS (
+	select idtrabajador,count(idficha) as total
+    from Ficha f 
+    group by idtrabajador
+)
+select t.usuario as USUARIO, ltrim(p.nombres) as NOMBRES, ltrim(p.apellidos) as APELLIDOS,
+	   (select count(idficha) from Ficha f where f.idtrabajador=t.idtrabajador) as TOTAL,
+       ROW_NUMBER() OVER(ORDER BY rt.total DESC) as RN,
+       RANK() OVER(ORDER BY rt.total DESC) as RK,
+       DENSE_RANK() OVER(ORDER BY rt.total DESC) as DRK
+from Trabajador t left join Padron p on t.idpadron=p.idpadron
+left join CTE_RT rt on t.idtrabajador=rt.idtrabajador
+where t.tipo='E'
+
+select * from V_REPORTE_ENCUESTADOR order by TOTAL DESC
+
+
+
+
+
